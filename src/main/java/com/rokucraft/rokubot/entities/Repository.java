4477bb4db4687fdc8946fe316bc8340ed4fa2d79
Aiko Vandeputte <aiko@aikovdp.me>
2022-08-30 22:13:41 +0200

    Remove unused code

--- src/main/java/com/rokucraft/rokubot/entities/Repository.java
+++ src/main/java/com/rokucraft/rokubot/entities/Repository.java
@@ -1,8 +1,6 @@
 package com.rokucraft.rokubot.entities;
 
-import com.rokucraft.rokubot.RokuBot;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Required;
 
@@ -25,17 +23,4 @@ public class Repository extends AbstractEntity{
         return repositoryUrl;
     }
 
-    @Nullable
-    public static Repository find(String name) {
-        return (Repository) find(name, RokuBot.getConfig().getRepositories());
-    }
-
-    @NonNull
-    public static Repository getDefault() {
-        Repository repository = find("default");
-        if (repository == null) {
-            repository = RokuBot.getConfig().getRepositories().get(0);
-        }
-        return repository;
-    }
 }
