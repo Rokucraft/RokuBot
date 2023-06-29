4a63eda3745117aead6c6f5a9f85c25aba23436a
Aiko Vandeputte <aiko@aikovdp.me>
2023-06-29 11:32:12 +0200

    Migrate utility classes to Kotlin

---
+++ src/main/java/com/rokucraft/rokubot/util/ColorConstants.kt
@@ -0,0 +1,10 @@
+package com.rokucraft.rokubot.util
+
+object ColorConstants {
+    const val BLUE = 0x42D9ED
+    const val FUCHSIA = 0xEB459E
+    const val GREEN = 0x57F287
+    const val ISSUE_OPEN_COLOR = 0x56D364
+    const val ISSUE_CLOSED_COLOR = 0xF85149
+    const val RED = 0xED4245
+}
---
+++ src/main/java/com/rokucraft/rokubot/util/EmbedUtil.kt
@@ -0,0 +1,14 @@
+package com.rokucraft.rokubot.util
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.MessageEmbed
+
+object EmbedUtil {
+    @JvmStatic
+    fun createErrorEmbed(description: String): MessageEmbed {
+        return EmbedBuilder()
+                .setColor(ColorConstants.RED)
+                .setDescription("❌ $description")
+                .build()
+    }
+}
