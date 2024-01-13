8041fa70429337c2030b4977f154e3e25f27a0c8
Aiko Vandeputte <aiko@aikovdp.me>
2024-01-13 14:49:18 +0100

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,14 +21,14 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-beta.11") {
+    implementation("net.dv8tion:JDA:5.0.0-beta.19") {
         exclude(module = "opus-java")
     }
-    implementation("ch.qos.logback:logback-classic:1.4.7")
-    implementation("org.kohsuke:github-api:1.315")
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("org.kohsuke:github-api:1.318")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
     implementation("org.spongepowered:configurate-yaml:4.1.2")
     implementation("org.spongepowered:configurate-extra-kotlin:4.1.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
