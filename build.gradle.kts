fe7e40664e85ddcad9a489325333330f1420da14
Aiko Vandeputte <aiko@aikovdp.me>
2024-01-13 13:49:12 +0100

    Upgrade Jib to 3.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.8.22"
-    id("com.google.cloud.tools.jib") version "3.3.2"
+    id("com.google.cloud.tools.jib") version "3.4.0"
 }
 
 application {
