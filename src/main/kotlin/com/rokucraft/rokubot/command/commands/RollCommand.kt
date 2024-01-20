7193a1cc3639c03ed1012ac59d7eb15f28718d89
Aiko Vandeputte <aiko@aikovdp.me>
2024-01-20 23:31:04 +0100

    Idiomatic Kotlin slay

--- src/main/kotlin/com/rokucraft/rokubot/command/commands/RollCommand.kt
+++ src/main/kotlin/com/rokucraft/rokubot/command/commands/RollCommand.kt
@@ -5,19 +5,16 @@ import com.rokucraft.rokubot.util.ColorConstants
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.build.Commands
-import java.security.SecureRandom
-import java.util.*
 
 class RollCommand : AbstractCommand() {
     override val data = Commands.slash("roll", "Roll a 20-sided dice")
-    private val random: Random = SecureRandom()
 
     override fun execute(event: SlashCommandInteractionEvent) {
         event.replyEmbeds(
             EmbedBuilder()
                 .setColor(ColorConstants.GREEN)
                 .addField("Roll", "1d20", true)
-                .addField("Result", random.nextInt(1, 21).toString(), true)
+                .addField("Result", (1..21).random().toString(), true)
                 .build()
         ).queue()
     }
