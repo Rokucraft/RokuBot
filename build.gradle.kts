45dc9c138b267c236782d15c202d27e51bbfee60
Aiko Vandeputte <aiko@aikovdp.me>
2024-09-06 13:42:18 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     application
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
-    id("com.google.cloud.tools.jib") version "3.4.2"
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
+    id("com.google.cloud.tools.jib") version "3.4.3"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
 }
 
 application {
@@ -22,20 +22,20 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-beta.23") {
+    implementation("net.dv8tion:JDA:5.1.0") {
         exclude(module = "opus-java")
     }
-    implementation("ch.qos.logback:logback-classic:1.5.6")
-    implementation("org.kohsuke:github-api:1.321")
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    implementation("ch.qos.logback:logback-classic:1.5.7")
+    implementation("org.kohsuke:github-api:1.324")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation("org.spongepowered:configurate-yaml:4.1.2")
     implementation("org.spongepowered:configurate-extra-kotlin:4.1.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
 
-    implementation("com.google.dagger:dagger:2.51.1")
-    ksp("com.google.dagger:dagger-compiler:2.51.1")
+    implementation("com.google.dagger:dagger:2.52")
+    ksp("com.google.dagger:dagger-compiler:2.52")
 }
 
 jib.to.image = "ghcr.io/rokucraft/rokubot:latest"
